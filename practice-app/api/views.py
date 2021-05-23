b93ad0509d8a07a6fd285032e7308e82a9f4ca8d
kerimoglutolga <tolgakerimoglu@outlook.com>
2021-05-23 10:48:47 +0300

    django rest framework integration

--- practice-app/api/views.py
+++ practice-app/api/views.py
@@ -1,3 +1,14 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+def apiOverview(request):
+    urls = {
+        'Login' : '/login/',
+        'Register' : '/register/'
+    }
+    return Response(urls)
