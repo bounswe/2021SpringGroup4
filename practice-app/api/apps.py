bc673cc7c54bc13975a21da3bc2d8023a5bf7bdb
kerimoglutolga <tolgakerimoglu@outlook.com>
2021-05-23 10:19:59 +0300

    some renaming and settings changes such as timezone setup

--- practice-app/api/apps.py
+++ practice-app/api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PracticeappConfig(AppConfig):
+class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'practiceapp'
+    name = 'api'
