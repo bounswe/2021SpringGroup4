b211d577776dffef66b3f70f6970aab46abd335f
kerimoglutolga <tolgakerimoglu@outlook.com>
2021-06-05 15:45:52 +0300

    removed register

--- practice-app/api/register/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class RegistrationForm(forms.Form):
-    username = forms.CharField(label='Username', max_length=30)
-    password = forms.CharField(label='Password', max_length=256) 
-    email = forms.EmailField(label='E-mail', max_length=50)
-    description = forms.CharField(label='Description', max_length=250)
-    age = forms.IntegerField(label='Age')
-    location = forms.CharField(label='Location', max_length=60)
