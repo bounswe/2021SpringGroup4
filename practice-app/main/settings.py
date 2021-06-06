1dd4887649c8f2f05e8607cc52fe05fa8c8d3ed5
kerimoglutolga <tolgakerimoglu@outlook.com>
2021-06-06 21:40:51 +0300

    dockerfile

--- practice-app/main/settings.py
+++ practice-app/main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6_sbco#dl-43-&_gce1vh1&(ya4vl@-=+qs7i-akjxf1dzq2b5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
