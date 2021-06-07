84782a2a141d7670ff13df75f227e28606a89f25
kerimoglutolga <tolgakerimoglu@outlook.com>
2021-06-08 00:23:36 +0300

    index.html

--- practice-app/main/urls.py
+++ practice-app/main/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
-
+from api import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('', view=views.index)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
