1985fc91e75ff1009a3610bb5aa3d03a668a0a36
yigitsarioglu <81223660+yigitsarioglu@users.noreply.github.com>
2021-06-10 22:20:33 +0300

    Update main.py
    I have added some code documentation (comments) to covid_reports/main.py file.  code doesn't changed.

--- practice-app/api/covid_reports/main.py
+++ practice-app/api/covid_reports/main.py
@@ -29,39 +29,44 @@ from django.http import HttpResponse
 #This method returns the data of the all over the world
 #Also users could search specific country
 def covid_api (request):
-    covid19 = COVID19Py.COVID19()
+    covid19 = COVID19Py.COVID19()    #create a new instance of covid19
 
     if request.method == 'GET':
         form=SearchForm()       #initialize a form object
 
-        latest = covid19.getLatest()
-        confirmed= latest["confirmed"]
-        death = latest["deaths"]
-        recover=latest["recovered"]
+        latest = covid19.getLatest()   #Getting latest amount of total confirmed cases, deaths, and recoveries
+        confirmed= latest["confirmed"]  #latest amount of total confirmed case all over the world
+        death = latest["deaths"]        #latest amount of total deaths all over the world
+        recover=latest["recovered"]     #latest amount of total recover all over the world
 
-        deathranks = covid19.getLocations(rank_by='deaths')
-        confirmedranks = covid19.getLocations(rank_by='confirmed')
+        deathranks = covid19.getLocations(rank_by='deaths')           #  rank the results deaths 
+        confirmedranks = covid19.getLocations(rank_by='confirmed')    #  rank the results confirmed
+        #Here list1 takes the name of the top 20 countries according to death
         list1=[]
         for x in range(20) :
             list1.append(deathranks[x]["country"])
-
+            
+        #Here list2 takes the name of the top 20 countries according to confirmed case
         list2=[]
         for x in range(20) :
             list2.append(confirmedranks[x]["country"])
-
+        
+        # Returns the search form for the user, and the latest amount of total confirmed cases, deaths, and recoveries information,
+        # Also returns death rankings and confirmed case rankings of countries 
         return render(request, 'covid_reports.html', {'confirm' : confirmed, 'recovery' : recover,
                                                       'death' : death, 'sform':form, "deathrank":list1, "confirmrank":list2 })
 
     elif request.method == 'POST':
-        form = SearchForm(request.POST)
+        form = SearchForm(request.POST)  #initialize a form object
         if form.is_valid():
-            country = form.cleaned_data.get('country')
-            if len(country) != 2:
+            country = form.cleaned_data.get('country')   #gets the country code from the form
+            if len(country) != 2:                #if the country code length is not equal to 2. ıt is not a valid countrycode. so it returns HttpResponse
                 return HttpResponse("<h1>Not Valid Country Code..Code length should be 2 .Please write valid code</h1>")
-
-            elif pycountry.countries.get(alpha_2=country):
-                return covid_country_api(request, country)
-            else:
+            
+            #Using pycountry package, specific countries can be looked up by their various codes 
+            elif pycountry.countries.get(alpha_2=country):    #checks the country code, with using pycountry
+                return covid_country_api(request, country)  # if it is valid, it  calls the covid_country_api method to show the specific country data
+            else:  #if that country code is not valid, HttpResponse sended
                 return HttpResponse("<h1>Not Valid Country Code..Your Country Code is not in the table. Please look the country table. Then write a valid code</h1>")
 
         else:
@@ -76,43 +81,46 @@ def covid_api (request):
 
 
 
-#This method returns selected country covid19 data(confirmed,death,recover)
-# Works with GET call.
+#This method returns selected country covid19 data(confirmed,death,recovered)
 def covid_country_api (request,countrycode):
 
-    covid19 = COVID19Py.COVID19()
-    if request.method == 'GET':
+    covid19 = COVID19Py.COVID19()   #Create a new instance of covid19
+    # GET method : Users could search the country using :  /covid19/countrycode  
+    # for example: /covid19/TR  calls the "GET" and returns the turkey covid data
+    if request.method == 'GET':  #If GET method is called
 
-        if len(countrycode) != 2:
+        if len(countrycode) != 2:    #a valid country code should be 2.this statement check this condition
             return HttpResponse("<h1>Not Valid Country Code..Code length should be 2 .Please write valid code</h1>")
 
-        elif pycountry.countries.get(alpha_2=countrycode):
+        elif pycountry.countries.get(alpha_2=countrycode):  #checks the country code, with using pycountry 
 
-            locationdata = covid19.getLocationByCountryCode(countrycode)
-            countryname = locationdata[0]["country"]
-            confirmed = locationdata[0]["latest"]["confirmed"]
-            deaths = locationdata[0]["latest"]["deaths"]
-            recovered = locationdata[0]["latest"]["recovered"]
-            updatetime = locationdata[0]["last_updated"]
-
+            locationdata = covid19.getLocationByCountryCode(countrycode)  #get the data according to specific country code
+            countryname = locationdata[0]["country"]                      # gets the country name data
+            confirmed = locationdata[0]["latest"]["confirmed"]            # gets the country confirmed cases
+            deaths = locationdata[0]["latest"]["deaths"]                 # gets the country death data
+            recovered = locationdata[0]["latest"]["recovered"]           # gets the country recovered  data
+            updatetime = locationdata[0]["last_updated"]                  # gets the update time
+            
+            #Returns the confirmed cases,deaths ,recovery , update time of data and country name
             return render(request, 'covid_country_report.html',
                       {'cname': countryname, 'confirm': confirmed, 'death': deaths, 'recover': recovered,
                        'time': updatetime})
-        else:
+        else:  #if that country code is not valid, HttpResponse sended
             return HttpResponse(
                 "<h1>Not Valid Country Code..Your Country Code is not in the table. Please look the country table. Then write a valid code</h1>")
 
 
-
+     # POST method : called when user post a request
     elif request.method == 'POST':
 
-        locationdata = covid19.getLocationByCountryCode(countrycode)
-        countryname = locationdata[0]["country"]
-        confirmed = locationdata[0]["latest"]["confirmed"]
-        deaths = locationdata[0]["latest"]["deaths"]
-        recovered = locationdata[0]["latest"]["recovered"]
-        updatetime = locationdata[0]["last_updated"]
+        locationdata = covid19.getLocationByCountryCode(countrycode)  
+        countryname = locationdata[0]["country"]                      # gets the country name data
+        confirmed = locationdata[0]["latest"]["confirmed"]            # gets the country confirmed cases
+        deaths = locationdata[0]["latest"]["deaths"]                 # gets the country death data
+        recovered = locationdata[0]["latest"]["recovered"]          # gets the country recovered  data
+        updatetime = locationdata[0]["last_updated"]                 #gets the update time
 
+        #Returns the confirmed cases,deaths ,recovery , update time of data and country name
         return render(request, 'covid_country_report.html',
                       {'cname': countryname, 'confirm': confirmed, 'death': deaths, 'recover': recovered,
                        'time': updatetime})
