91d12bc853739f8b272adb9cb505347c2324df06
Salih Furqan <71407287+salihfurqan@users.noreply.github.com>
2021-06-10 14:10:52 +0300

    remove '/api' from response

--- practice-app/api/eq_post/test_eq_post.py
+++ practice-app/api/eq_post/test_eq_post.py
@@ -10,7 +10,7 @@ class PostTests(APITestCase):
         Ensure post is valid.
         """
         data = { 'username': 'test_user', 'title': 'test_title', 'description': 'I am a test description', 'location': 'test_location'}
-        response = self.client.post('/api/eq_post/', data, format='json')
+        response = self.client.post('/eq_post/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_valid_post_with_extra_fields(self):
@@ -18,7 +18,7 @@ class PostTests(APITestCase):
         Ensure post is valid with additional fields appended to the request data.
         """
         data = { 'username': 'test_user', 'title': 'test_title', 'description': 'I am a test description', 'location': 'test_location', 'useless': 'useless field'}
-        response = self.client.post('/api/eq_post/', data, format='json')
+        response = self.client.post('/eq_post/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_null_username(self):
@@ -26,7 +26,7 @@ class PostTests(APITestCase):
         Ensure we don't allow posts without a username.
         """
         data = { 'username': '', 'title': 'test_title', 'description': 'I am a test description', 'location': 'test_location'}
-        response = self.client.post('/api/eq_post/', data, format='json')
+        response = self.client.post('/eq_post/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_null_title(self):
@@ -34,7 +34,7 @@ class PostTests(APITestCase):
         Ensure we don't allow registrations without a title.
         """
         data = { 'username': 'test_user', 'title': '', 'description': 'I am a test description', 'location': 'test_location',}
-        response = self.client.post('/api/eq_post/', data, format='json')
+        response = self.client.post('/eq_post/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST) 
 
     def test_incomplete_registration(self):
@@ -42,5 +42,5 @@ class PostTests(APITestCase):
         Ensure that users can post equipment without entering location or a description.
         """
         data = { 'username': 'test_user', 'title': 'test_title', 'description': '', 'location': ''}
-        response = self.client.post('/api/eq_post/', data, format='json')
+        response = self.client.post('/eq_post/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
