6614337960a51943213673c73eb84e7f20e1bbfb
yigitsarioglu <81223660+yigitsarioglu@users.noreply.github.com>
2021-06-10 22:08:20 +0300

    Uptdate test_covid.py
    we have hanged the link structure in the app, so I changed url's of /covid19

--- practice-app/api/covid_reports/test_covid.py
+++ practice-app/api/covid_reports/test_covid.py
@@ -8,7 +8,7 @@ class Covid19Tests(APITestCase):
         Ensure we could search a valid country
         """
         data = {'TR'}
-        response = self.client.post('/api/covid19/', data, format='json')
+        response = self.client.post('/covid19/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK, " not success status response code")
         print("Test completed")
 
@@ -17,7 +17,7 @@ class Covid19Tests(APITestCase):
         Ensure we could not search a unvalid country
         """
         data = {'TRZ'}                   # valid country code length should be 2
-        response = self.client.post('/api/covid19/', data, format='json')
+        response = self.client.post('/covid19/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK , " not success status response code")
         print("Test completed")
 
@@ -27,7 +27,7 @@ class Covid19Tests(APITestCase):
         Ensure we could not search a unvalid country
         """
         data = {'ZZ'}     # no any country code with ZZ
-        response = self.client.post('/api/covid19/', data, format='json')
+        response = self.client.post('/covid19/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK , " not success status response code")
         print("Test completed")
 
@@ -35,6 +35,6 @@ class Covid19Tests(APITestCase):
         """
         Ensure we have web connection to take data from source
         """
-        response=self.client.get('/api/covid19/')
+        response=self.client.get('/covid19/')
         self.assertEqual(response.status_code, status.HTTP_200_OK, " not success status response code")
         print("Test completed")
