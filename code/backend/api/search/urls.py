db63dfe28733d85d426dbd10d72ce1cfc2ca3c6e
Tolga Kerimoğlu <tolgakerimoglu@outlook.com>
2021-11-13 20:41:15 +0300

    event search with location services

--- code/backend/api/search/urls.py
+++ code/backend/api/search/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import UserSearch
+from .views import UserSearch, EventSearch
 
 urlpatterns = [
     path('user/<field>/<value>', UserSearch.as_view(), name="user_search"),
+    path('event/<field>/', EventSearch.as_view(), name="event_search"),
 ]
 
--- code/backend/api/search/views.py
+++ code/backend/api/search/views.py
@@ -9,7 +9,7 @@ from rest_framework.permissions import AllowAny
 from django.db.models import Q
 from rest_framework.response import Response
 from rest_framework import status
-
+from .utils import distance, dist, loc
 class UserSearch(views.APIView):
     def get(self, request, *args, **kwargs):
         if kwargs.get('field') == 'username':
@@ -20,3 +20,32 @@ class UserSearch(views.APIView):
             qs = User.objects.filter(Q(first_name__contains=kwargs.get('value')))
             serializer = UserSerializer(qs, many=True)
             return Response(status=status.HTTP_200_OK, data=serializer.data)  
+
+class EventSearch(views.APIView):
+    permission_classes = [AllowAny]
+    def post(self, request, *args, **kwargs):
+        if kwargs.get('field') == 'location':
+            location = request.data.get('location', None) 
+            filter_dist = request.data.get('dist', None)
+            lat, lon = loc(location)
+            ids = []
+            for event in EventBody.objects.all().only('id', 'location'):
+                id, event_loc = event.id, event.location
+                try:
+                    lat2, lon2 = loc(event_loc)
+                except IndexError:
+                    continue
+
+                dist2src = distance(lat, lat2, lon, lon2) 
+                if dist2src < int(filter_dist):
+                    ids.append(id)
+            return_events = []
+            qs = EventBody.objects.filter(Q(id__in=ids))
+            for event in qs:
+                data = {}
+                data['owner'] = event.parent.owner.username 
+                data['id'] = event.id 
+                data['title'] = event.title 
+                data['location'] = event.location 
+                return_events.append(data)
+            return Response(status=200, data=return_events, content_type='json')
