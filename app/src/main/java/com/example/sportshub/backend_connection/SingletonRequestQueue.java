927f18393d877f00f7f810a1490538e0105c6dd6
eceasslan <ece.aslan19@gmail.com>
2021-11-14 00:33:41 +0300

    Created singleton class of RequestQueue

---
+++ app/src/main/java/com/example/sportshub/backend_connection/SingletonRequestQueue.java
@@ -0,0 +1,39 @@
+package com.example.sportshub.backend_connection;
+
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class SingletonRequestQueue {
+
+    private static SingletonRequestQueue instance;
+    private RequestQueue requestQueue;
+    private static Context context;
+
+    private SingletonRequestQueue(Context context) {
+        this.context = context;
+        requestQueue = getRequestQueue();
+    }
+
+    public static synchronized SingletonRequestQueue getInstance(Context context) {
+        if (instance == null) {
+            instance = new SingletonRequestQueue(context);
+        }
+        return instance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(context.getApplicationContext());
+        }
+        return requestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+}
