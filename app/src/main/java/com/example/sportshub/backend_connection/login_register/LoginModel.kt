bfb8bc60e48ad6ed3916085fccd5079027171fbe
eceasslan <ece.aslan19@gmail.com>
2021-11-14 18:01:59 +0300

    Created LoginModel and RegisterModel

---
+++ app/src/main/java/com/example/sportshub/backend_connection/login_register/LoginModel.kt
@@ -0,0 +1,6 @@
+package com.example.sportshub.backend_connection.login_register
+
+data class LoginModel(
+    val username: String,
+    val password: String
+)
---
+++ app/src/main/java/com/example/sportshub/backend_connection/login_register/RegisterModel.kt
@@ -0,0 +1,8 @@
+package com.example.sportshub.backend_connection.login_register
+
+data class RegisterModel(
+    val email: String,
+    val username: String,
+    val password: String,
+    val name: String
+)
