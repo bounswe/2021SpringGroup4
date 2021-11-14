8813bdf9627d00c9d3459008d22930e7aa2a22e5
eceasslan <ece.aslan19@gmail.com>
2021-11-15 00:17:50 +0300

    Organized fields according to API and added default values to EventModel and SportTypeModel files

--- app/src/main/java/com/example/sportshub/backend_connection/event/EventModel.kt
+++ app/src/main/java/com/example/sportshub/backend_connection/event/EventModel.kt
@@ -1,12 +1,19 @@
 package com.example.sportshub.backend_connection.event
 
 data class EventModel(
-    var name: String,
-    var location: String,
-    var date: String,
-    var time: String,
-    var duration: Int, // unit-> minutes
-    var sport_type: SportTypeModel,
-    var number_of_people: Int,
-    var skill_level: Float  // score out of 5
+    var id: Int = 0,
+    var creator: String = "",
+    var title: String = "",
+    var description: String = "",
+    var date: String = "",
+    var time: String = "",
+    var duration: String = "",
+    var location: String = "",
+    var sportType: SportTypeModel = SportTypeModel(),
+    var maxPlayers: Int = 0,
+    var applicants: MutableList<String> = mutableListOf(),
+    var participants: MutableList<String> = mutableListOf(),
+    var followers: MutableList<String> = mutableListOf(),
+    var comments: MutableList<String> = mutableListOf(),
+    var skillLevel: Float = 0F  // score out of 5
 )
