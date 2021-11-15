92a1f6d6207391553620a3a44c9c10c93a9dc284
MEHMET HİLMİ DÜNDAR <mehmethilmidundar@gmail.com>
2021-11-15 03:40:25 +0300

    test_equipment_update is added in equipment tests.py

--- code/backend/api/equipment/tests.py
+++ code/backend/api/equipment/tests.py
@@ -29,6 +29,7 @@ class EquipmentTests(APITestCase):
         self.assertEqual(event.location, "Test Equipment Location")   
         self.assertEqual(event.description, "This equipment is created for testing")
 
+
     def test_equipment_remove(self):
         user = User.objects.create(username="eqp_test_add_remove", password="12345",
                                     email="eqp_test@email.com")
@@ -49,6 +50,7 @@ class EquipmentTests(APITestCase):
 
         response = self.client.delete(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)       
+
 
     def test_equipment_many(self):
         user = User.objects.create(username="test_equipment_many", password="12345",
@@ -79,3 +81,33 @@ class EquipmentTests(APITestCase):
             eqp_counter += 1
         
         self.assertEqual(eqp_counter, EquipmentPost.objects.all().count())        
+
+
+    def test_equipment_update(self):
+        user = User.objects.create(username="eqp_test_update", password="12345",
+                                   email="eqp_test@email.com")
+        self.client.force_authenticate(user=user)
+
+        #creating eqp
+        data = {
+            "title": "test_equipment_update",
+            "location": "Test Equipment Location",
+            "description": "This equipment is created for testing",     
+        }
+        url = reverse('equipment_list')
+        response = self.client.post(url, data=data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        #updating eqp
+        eqp_id = response.data.get('id')
+        url2 = reverse('equipment_rud', kwargs={'pk': eqp_id})
+
+        data2 = {
+            "description": "This equipment is created for testing. It is updated",     
+        }        
+
+        response = self.client.patch(url2, data=data2 ,format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data.get("description"), "This equipment is created for testing. It is updated")     
