5962a393fc120104d2a051e08017dfbd7c72b2d6
alialperendurak <ali.durak@boun.edu.tr>
2021-12-02 13:34:53 +0300

    unnecessary files are removed and main branch of frontend is created as frontendmain

--- code/backend/api/profiles/serializers.py
+++
@@ -1,8 +0,0 @@
-from api.authentication.models import User 
-from rest_framework import serializers
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name', 
-                'age', 'about', 'location', 'going', 'applied', 'following']
