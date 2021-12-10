ed743fd8e72d06ca2038edfd87991ce6e830e9ce
eceasslan <ece.aslan19@gmail.com>
2021-12-10 20:38:31 +0300

    Fixed event list UI

--- app/src/main/java/com/example/sportshub/ui/event/EventAdapter.kt
+++ app/src/main/java/com/example/sportshub/ui/event/EventAdapter.kt
@@ -9,12 +9,25 @@ import com.example.sportshub.R
 import com.example.sportshub.backend_connection.event.EventModel
 
 class EventAdapter:RecyclerView.Adapter<EventAdapter.EventViewHolder>() {
-    var eventList: MutableList<EventModel> = MutableList<EventModel>(0, init = {index -> EventModel()})
+    var eventList: MutableList<EventModel> = mutableListOf()
 
     class EventViewHolder(view: View):RecyclerView.ViewHolder(view) {
+        var months = listOf("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
         private val title: TextView = view.findViewById(R.id.event_title)
-        fun bind(string: String){
-            title.text = string
+        private val location: TextView = view.findViewById(R.id.event_location)
+        private val date: TextView = view.findViewById(R.id.event_date)
+        private val time: TextView = view.findViewById(R.id.event_time)
+        private val players: TextView = view.findViewById(R.id.event_players)
+        private val creator: TextView = view.findViewById(R.id.event_creator)
+        fun bind(eventModel: EventModel){
+            title.text = eventModel.title
+            location.text = eventModel.location
+            val dateParse = eventModel.date.split("-")
+            date.text = "${dateParse[2]} ${months[dateParse[1].toInt()-1]} ${dateParse[0]}"
+            val timeParse = eventModel.time.split(":")
+            time.text = "${timeParse[0]}:${timeParse[1]}"
+            players.text = "${eventModel.participants.size} out of ${eventModel.maxPlayers} players are chosen"
+            creator.text = "${eventModel.creator} created this event"
         }
     }
 
@@ -26,7 +39,7 @@ class EventAdapter:RecyclerView.Adapter<EventAdapter.EventViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
-        holder.bind(eventList[position].toString())
+        holder.bind(eventList[position])
     }
 
     override fun getItemCount(): Int {
