a082cfd5f3d2d06597e065610e95f954093e7238
Fatih Akgöz <44064984+FatihAkgoz@users.noreply.github.com>
2021-12-12 23:52:54 +0300

    added Date and Time pickers to CreateEventFragment

--- app/src/main/java/com/example/sportshub/event/EventCreateFragment.kt
+++ app/src/main/java/com/example/sportshub/event/EventCreateFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.TimePicker
 import com.example.sportshub.R
 
 class EventCreateFragment : Fragment() {
@@ -20,15 +22,22 @@ class EventCreateFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.event_create_fragment, container, false)
 
+        return view
+    }
 
-        return inflater.inflate(R.layout.event_create_fragment, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TimePicker>(R.id.editTextTime).setIs24HourView(true)
+        view.findViewById<TimePicker>(R.id.editTextDuration).setIs24HourView(true)
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(EventCreateViewModel::class.java)
-        // TODO: Use the ViewModel
+
     }
 
 }
