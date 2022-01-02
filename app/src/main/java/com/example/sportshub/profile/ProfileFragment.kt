4827c46609dce847eb59c1b6ca166efc2dda23f3
eceasslan <ece.aslan19@gmail.com>
2022-01-02 16:05:30 +0300

    Search profile functionality

--- app/src/main/java/com/example/sportshub/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/sportshub/profile/ProfileFragment.kt
@@ -47,45 +47,65 @@ class ProfileFragment : Fragment() {
                 }
             })
 
+        val rw_upcoming: RecyclerView = binding.listUpcomingEvents
+        val adapter_upcoming = ProfileEventAdapter()
+        rw_upcoming.adapter = adapter_upcoming
+        rw_upcoming.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
+        val rw_applied: RecyclerView = binding.listAppliedEvents
+        val adapter_applied = ProfileEventAdapter()
+        rw_applied.adapter = adapter_applied
+        rw_applied.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
+        val rw_badges: RecyclerView = binding.listBadges
+        val adapter_badges = BadgeAdapter()
+        rw_badges.adapter = adapter_badges
+        rw_badges.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
+
         profile.observe(viewLifecycleOwner, {
             binding.username.text = it.username
             if(it.first_name == ""){
                 binding.firstName.isVisible = false
             } else{
+                binding.firstName.isVisible = true
                 binding.firstName.text = it.first_name
             }
             if(it.last_name == ""){
                 binding.lastName.isVisible = false
             } else{
+                binding.lastName.isVisible = true
                 binding.lastName.text = it.last_name
             }
             if(it.age == 0){
                 binding.age.isVisible = false
             } else{
+                binding.age.isVisible = true
                 binding.age.text = it.age.toString()
             }
             if(it.location == ""){
                 binding.location.isVisible = false
             } else{
+                binding.location.isVisible = true
                 binding.location.text = it.location
             }
             if(it.about == ""){
                 binding.about.isVisible = false
             } else{
+                binding.about.isVisible = true
                 binding.about.text = it.about
             }
             if(it.going.size == 0){
+                binding.noUpcoming.isVisible = true
                 binding.listUpcomingEvents.isVisible = false
                 val params = binding.appliedEventsText.layoutParams as ConstraintLayout.LayoutParams
                 params.topToBottom = binding.noUpcoming.id
                 params.topMargin = 64
                 binding.appliedEventsText.requestLayout()
             } else{
+                binding.listUpcomingEvents.isVisible = true
                 binding.noUpcoming.isVisible = false
-                val rw_upcoming: RecyclerView = binding.listUpcomingEvents
-                val adapter_upcoming = ProfileEventAdapter()
-                rw_upcoming.adapter = adapter_upcoming
-                rw_upcoming.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
+                val params = binding.appliedEventsText.layoutParams as ConstraintLayout.LayoutParams
+                params.topToBottom = binding.listUpcomingEvents.id
+                params.topMargin = 0
+                binding.appliedEventsText.requestLayout()
                 var upcoming_event_list: MutableList<EventModel> = mutableListOf()
                 for(i in 0 until it.going.size){
                     profileViewModel.getEventDetail(requireContext(), it.going.get(i),
@@ -105,17 +125,19 @@ class ProfileFragment : Fragment() {
                 }
             }
             if(it.applied.size == 0){
+                binding.noApplied.isVisible = true
                 binding.listAppliedEvents.isVisible = false
                 val params = binding.badgesText.layoutParams as ConstraintLayout.LayoutParams
                 params.topToBottom = binding.noApplied.id
                 params.topMargin = 64
                 binding.badgesText.requestLayout()
             } else{
+                binding.listAppliedEvents.isVisible = true
                 binding.noApplied.isVisible = false
-                val rw_applied: RecyclerView = binding.listAppliedEvents
-                val adapter_applied = ProfileEventAdapter()
-                rw_applied.adapter = adapter_applied
-                rw_applied.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
+                val params = binding.badgesText.layoutParams as ConstraintLayout.LayoutParams
+                params.topToBottom = binding.listAppliedEvents.id
+                params.topMargin = 0
+                binding.badgesText.requestLayout()
                 var applied_event_list: MutableList<EventModel> = mutableListOf()
                 for(i in 0 until it.applied.size){
                     profileViewModel.getEventDetail(requireContext(), it.applied.get(i),
@@ -135,17 +157,36 @@ class ProfileFragment : Fragment() {
                 }
             }
             if(it.badges_list.size == 0){
+                binding.noBadges.isVisible = true
                 binding.listBadges.isVisible = false
             } else{
+                binding.listBadges.isVisible = true
                 binding.noBadges.isVisible = false
-                val rw_badges: RecyclerView = binding.listBadges
-                val adapter_badges = BadgeAdapter()
-                rw_badges.adapter = adapter_badges
-                rw_badges.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
                 adapter_badges.badgeList = it.badges_list
                 adapter_badges.notifyDataSetChanged()
             }
         })
+
+        binding.btnSearchUser.setOnClickListener {
+            if(binding.editTextSearchUser.text.toString() == ""){
+                Toast.makeText(requireContext(),"Please don't leave the username blank!", Toast.LENGTH_SHORT).show()
+            } else{
+                profileViewModel.getProfile(requireContext(), binding.editTextSearchUser.text.toString(),
+                    object: GetProfileListener() {
+                        override fun onError(statusCode: Int?) {
+                            if(statusCode == 404){
+                                Toast.makeText(requireContext(),"There is no user with the given username!", Toast.LENGTH_SHORT).show()
+                            } else{
+                                Toast.makeText(requireContext(),"Something wrong. Please try again!", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+
+                        override fun onResponse(profileModel: ProfileModel) {
+                            profile.value = profileModel
+                        }
+                    })
+            }
+        }
 
         return root
     }
