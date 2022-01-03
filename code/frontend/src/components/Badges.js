449f9f1ca0c4f854a1a2e4de959d660171115d8e
yagmurse <yselek14@gmail.com>
2022-01-03 21:21:55 +0300

    badges

---
+++ code/frontend/src/components/Badges.js
@@ -0,0 +1,15 @@
+import React, {useContext} from 'react'
+import { Link } from 'react-router-dom'
+import AuthContext from '../context/AuthContext'
+
+const Badges = () => {
+    
+    return (
+        <div>
+            
+           
+        </div>
+    )
+}
+
+export default Badges
